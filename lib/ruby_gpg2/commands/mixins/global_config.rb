7d64dcf51ee757aaf746b25f41f0997bea8e3634
Toby Clemson <tobyclemson@gmail.com>
2020-05-02 14:37:04 +0100

    Use no TTY by default.

--- lib/ruby_gpg2/commands/mixins/global_config.rb
+++ lib/ruby_gpg2/commands/mixins/global_config.rb
@@ -4,10 +4,12 @@ module RubyGPG2
       module GlobalConfig
         def configure_command(builder, opts)
           home_directory = opts[:home_directory]
+          without_tty = opts[:without_tty].nil? ? true : opts[:without_tty]
 
           builder = super(builder, opts)
           builder = builder.with_option(
               '--homedir', home_directory, quoting: '"') if home_directory
+          builder = builder.with_flag('--no-tty') if without_tty
           builder
         end
       end
--- spec/support/shared_examples/global_config.rb
+++ spec/support/shared_examples/global_config.rb
@@ -34,4 +34,42 @@ shared_examples(
     command.execute(
         options.merge(home_directory: home_directory))
   end
+
+  it 'includes the no-tty flag by default' do
+    command = subject.class.new
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with(
+                /^#{binary}.* --no-tty .*#{command_string}$/,
+                any_args))
+
+    command.execute(options)
+  end
+
+  it 'includes the no-tty flag when without tty is true' do
+    command = subject.class.new
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with(
+                /^#{binary}.* --no-tty .*#{command_string}$/,
+                any_args))
+
+    command.execute(
+        options.merge(without_tty: true))
+  end
+
+  it 'does not include the no-tty flag when without tty is false' do
+    command = subject.class.new
+
+    expect(Open4).to(
+        receive(:spawn)
+            .with(
+                /^#{binary} ((?!--no-tty).)*#{command_string}$/,
+                any_args))
+
+    command.execute(
+        options.merge(without_tty: false))
+  end
 end
