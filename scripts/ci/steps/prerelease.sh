1adf7ef101bb2aaf4869e2de796e528a2a45f25e
Toby Clemson <tobyclemson@gmail.com>
2021-01-02 16:29:58 +0000

    Add new secrets. Add secrets management tasks.

--- scripts/ci/steps/prerelease.sh
+++ scripts/ci/steps/prerelease.sh
@@ -10,11 +10,6 @@ PROJECT_DIR="$( cd "$SCRIPT_DIR/../../.." && pwd )"
 cd "$PROJECT_DIR"
 
 ./go version:bump[pre]
-
-bundle install
-LAST_MESSAGE="$(git log -1 --pretty=%B)"
-git commit -a --amend -m "${LAST_MESSAGE} [ci skip]"
-
 ./go release
 
 git status
